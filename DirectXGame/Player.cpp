ed06028a4a56b141bc122b2af90e0afdedd6b8a5
MaruyamaYuuki <k023g0106@g.neec.ac.jp>
2024-10-10 14:13:44 +0900

    自キャラとゴールの当たり判定の実装

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -202,3 +202,29 @@ void Player::MapCollisionLeftside(CollisionMapInfo& info) {
 }
 
 void Player::Draw() { model_->Draw(worldTransform_, *camera_, &objectColor_);}
+
+Vector3 Player::GetWorldPosition() {
+	Vector3 worldPos;
+
+	worldPos.x = worldTransform_.translation_.x;
+	worldPos.y = worldTransform_.translation_.y;
+	worldPos.z = worldTransform_.translation_.z;
+
+	return worldPos;
+}
+
+AABB Player::GetAABB() {
+	Vector3 worldPos = GetWorldPosition();
+
+	AABB aabb;
+
+	aabb.min = {worldPos.x - kWidth / 2.0f, worldPos.y - kHeight / 2.0f, worldPos.z - kWidth / 2.0f};
+	aabb.max = {worldPos.x + kWidth / 2.0f, worldPos.y + kHeight / 2.0f, worldPos.z + kWidth / 2.0f};
+
+	return aabb;
+}
+
+
+void Player::CollisionGoal(const Goal* goal) {
+	(void)goal;
+}
